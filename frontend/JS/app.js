deec0b5405c2c98640bd0c14e10fdbb94f8bce47
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-15 00:32:35 +0100

    axios call and converted in array with json decode

--- frontend/JS/app.js
+++ frontend/JS/app.js
@@ -1,6 +1,16 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      songs: [],
+    };
+  },
+  // affettuo la chiamata axios in {get(per ottenere un dato) non in post (per inviarlo)} usando il path assoluto del file get-list.php
+  created() {
+    axios
+      .get("http://localhost/boolean/php-dischi-json/API/get-list.php")
+      .then((response) => {
+        this.songs = response.data;
+      });
   },
 });
 app.mount("#app");
